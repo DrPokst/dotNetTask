a761d65a52a20143ad6aec963b12da929ece1a9d
DrPokst <62307971+DrPokst@users.noreply.github.com>
2021-11-24 14:52:39 +0200

    Initialized EntityFramework
    Added first migrations and defined ConnectionString

---
+++ dotNetTask.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using dotNetTask.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotNetTask.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+        }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
