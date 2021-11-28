c09a053affafb4851886bded777fec26652f1515
DrPokst <62307971+DrPokst@users.noreply.github.com>
2021-11-28 19:36:56 +0200

    Added validations for dto nad entity

--- dotNetTask.API/Dtos/CreateEmployeeDto.cs
+++ dotNetTask.API/Dtos/CreateEmployeeDto.cs
@@ -1,21 +1,29 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using dotNetTask.API.Helpers;
 
 namespace dotNetTask.API.Dtos
 {
     public record CreateEmployeeDto
     {
         [Required]
-        public string FistName { get; set; }
+        [MaxLength(50),MinLength(1)]
+        public string FirstName { get; set; }
         [Required]
+        [MaxLength(50),MinLength(1)]
+        [NotEqual(nameof(FirstName), ErrorMessage ="LastName can not be equal to Firstname")]
         public string LastName { get; set; }
         [Required]
+        [AgeRange(18,70)]
         public DateTime BirtDate { get; set; }
-
+        [Required]
+        [ValidDate("2000/01/01", ErrorMessage = "Employment date can not be futute date and earlier than 2000-01-01")]
+        public DateTime EmploymentDate { get; set; }
         public string Boss { get; set; }
         [Required]
         public string HomeAddress { get; set; }
         [Required]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Value should be greater than or equal to 1")]
         public int CurrentSalary { get; set; }
         [Required]
         public EmployeeRoles Role { get; set; }
--- dotNetTask.API/Dtos/UpdateEmployeeDto.cs
+++ dotNetTask.API/Dtos/UpdateEmployeeDto.cs
@@ -1,31 +1,29 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations;
 using dotNetTask.API.Helpers;
 
-namespace dotNetTask.API.Entities
+namespace dotNetTask.API.Dtos
 {
-    public record Employee
+    public record UpdateEmployeeDto
     {
-        [Required]
-        public Guid Id { get; init; }
         [Required]
         [MaxLength(50),MinLength(1)]
         public string FirstName { get; set; }
         [Required]
         [MaxLength(50),MinLength(1)]
+        [NotEqual(nameof(FirstName), ErrorMessage ="LastName can not be equal to Firstname")]
         public string LastName { get; set; }
         [Required]
         [AgeRange(18,70)]
         public DateTime BirtDate { get; set; }
         [Required]
-        [ValidDate("2000/01/01")]
+        [ValidDate("2000/01/01", ErrorMessage = "Employment date can not be futute date and earlier than 2000-01-01")]
         public DateTime EmploymentDate { get; set; }
-        
-        public Employee Boss { get; set; }
+        public string Boss { get; set; }
         [Required]
         public string HomeAddress { get; set; }
         [Required]
-        [Range(1, Int32.MaxValue)]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Value should be greater than or equal to 1")]
         public int CurrentSalary { get; set; }
         [Required]
         public EmployeeRoles Role { get; set; }
